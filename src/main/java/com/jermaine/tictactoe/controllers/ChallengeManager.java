bb95429f46bef3b791abb20ddd517dacb62115a2
Jermaine <jermainec87@gmail.com>
2016-08-23 19:24:57 -0700

    update accept challenge text

--- src/main/java/com/jermaine/tictactoe/controllers/ChallengeManager.java
+++ src/main/java/com/jermaine/tictactoe/controllers/ChallengeManager.java
@@ -45,7 +45,7 @@ public class ChallengeManager {
                         .append("<@")
                         .append(challengedUser)
                         .append("> ")
-                        .append("type ```/ttt accept``` to startService the game!");
+                        .append("type ```/ttt accept``` to start the game!");
 
         return new SlackResponse()
                 .changeResponseTypeToInChannel()
